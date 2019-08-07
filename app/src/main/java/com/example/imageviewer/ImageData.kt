ffa0960a4df64f6e81317534bcd547349c83f918
stilljack <stilljack@gmail.comconfig --global user.email stilljack@gmail.com>
2019-08-07 15:47:14 -0700

    looks like i've got some more work to do

--- app/src/main/java/com/example/imageviewer/ImageData.kt
+++ app/src/main/java/com/example/imageviewer/ImageData.kt
@@ -2,13 +2,29 @@ package com.example.imageviewer
 
 import android.net.Uri
 import java.io.Serializable
-import java.net.URI
 
-class ImageData (imageURI: URI) : Serializable {
+class ImageData(fullPhotoUri: Uri) : Serializable {
 
-    val fileURIString: String  = imageURI.toString()
+
+        var name: String? = null
+        var description: String? = null
+        val fileUriString: String
+
+
+
     val fileUri: Uri
-        get() = Uri.parse(fileURIString)
+        get() = Uri.parse(fileUriString)
+    //gets raw URI string from FileUriStrong designates it a URI jss
 
+    init {
+
+        this.fileUriString = fullPhotoUri.toString()
+        //not going to pretend to understand in depth why this works but looks like a regular expression
+        //to snag the file name off the end of URI -- one day I'll have magic powers if i keep trying
+        val path = fullPhotoUri.path!!.split("/".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        this.name = path[path.size - 1]
+        //AFAICT this gets the second to last chunk of path to find the name of the file jss
+        this.description = ""
+    }
 }
 
--- app/src/main/java/com/example/imageviewer/MainActivity.kt
+++ app/src/main/java/com/example/imageviewer/MainActivity.kt
@@ -1,32 +1,63 @@
 package com.example.imageviewer
 
+import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.ArrayList
+import java.util.jar.Attributes
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val REQUEST_IMAGE_GET = 1
+        var index =0
+        var imageList: ArrayList<ImageData> = ArrayList()
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
 
+        add_button.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            if(intent.resolveActivity(packageManager) != null) {
+                startActivityForResult(intent, REQUEST_IMAGE_GET)
+            }
 
-    fun addTextView (text) {
-        for(i in 0..9) {
-            val view = createTextView(itemList[i], userList[i])
-            picture_linear_layout.addView(view)
+
         }
+
+
+
     }
-    fun createTextView(item: String, user: Int): TextView {
+
+
+    fun createTextView(string: String, int: Int): TextView {
         val view = TextView(this)
-
-        view.text = "${item.name} - ${item.price}, ${user.location}, ${user.reputation}"
+        view.text = string
         view.textSize = 24f
-
         return view
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        val fullPhotoUri = data!!.data
+
+        if (fullPhotoUri != null) {
+            imageList.add(ImageData(fullPhotoUri))
+            //stealing a bit, this is probably a terrible way to do it
+            val path = fullPhotoUri.path!!.split("/".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+            picture_linear_layout.addView(createTextView(path[path.size - 1], index))
+        }
+
+
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
